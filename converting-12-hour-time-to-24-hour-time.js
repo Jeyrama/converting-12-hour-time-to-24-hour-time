b655a4a3c010d37fa73f5ef908b159b239f65d1d
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-08-25 19:30:19 -0700

    Update converting-12-hour-time-to-24-hour-time.js

--- converting-12-hour-time-to-24-hour-time.js
+++ converting-12-hour-time-to-24-hour-time.js
@@ -17,3 +17,12 @@ Notes:
 
 
 // Solution
+
+function to24hourtime(hour, minute, period) {
+  if (period === "am" && hour === 12) {
+    hour = 0;
+  } else if (period === "pm" && hour !== 12) {
+    hour += 12;
+  }
+  return hour.toString().padStart(2, "0") + minute.toString().padStart(2, "0");
+}
